a5d479fb368ea99068dc0fb918be850f5e19a4ba
Sandip Chaudhary <sndp.c114@gmail.com>
2021-05-11 19:08:22 +0545

    Added some components and styling and refactored codes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   state = { term: '' };
+  // constructor(props) {
+  //   super(props);
 
+  //   this.state = {
+  //     term: '',
+  //   };
+  //   //step 1 to keep focus in input
+  //   this.inputRef = React.createRef();
+  // }
+
+  //step 2 to keep focus in input
+  // focusInput = () => {
+  //   this.inputRef.current.focus();
+  // };
+
+  //step 3 to keep focus in input and add ref in input tag
+  // componentDidMount() {
+  //   this.inputRef.current.focus();
+  // }
   //** callback function */
   //** Assigning arrow function to prevent the error in console */
   onFormSubmit = (event) => {
@@ -21,6 +39,7 @@ class SearchBar extends React.Component {
             <label>Search Pics</label>
             <input
               type="text"
+              // ref={this.inputRef}
               value={this.state.term}
               onChange={(e) => this.setState({ term: e.target.value })}
             />
